e9d39093efe7973f316f960c1b118baf675788b3
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2023-08-22 09:21:31 +0300

    service communication

--- inventory-service/src/main/java/com/orcrist/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/orcrist/inventoryservice/controller/InventoryController.java
@@ -1,9 +1,12 @@
 package com.orcrist.inventoryservice.controller;
 
+import com.orcrist.inventoryservice.dto.InventoryResponse;
 import com.orcrist.inventoryservice.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -12,9 +15,10 @@ public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    @GetMapping("/{sku-code}")
+    // example : http: //localhost:8082/api/inventory?skuCode=iphone-13&skuCode=iphone13-red
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
         return inventoryService.isInStock(skuCode);
     }
 }
--- order-service/src/main/java/com/orcrist/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/orcrist/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.orcrist.orderservice.service;
 
+import com.orcrist.orderservice.dto.InventoryResponse;
 import com.orcrist.orderservice.dto.OrderLineItemsDto;
 import com.orcrist.orderservice.dto.OrderRequest;
 import com.orcrist.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.orcrist.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,8 +21,9 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -30,7 +34,27 @@ public class OrderService {
 
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+
+        // Call Inventory Service, and place order if product is in stock
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream( inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later.");
+        }
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
